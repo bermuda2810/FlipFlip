60481938313e9399dfad0ea8ac8e2122a0fb111c
vietbq <vietbq@nal.vn>
2020-02-03 02:12:59 +0700

    Initial Commit

---
+++ FlipFlip/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FlipFlip
+//
+//  Created by Bui Quoc Viet on 2/3/20.
+//  Copyright © 2020 Class iOS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ FlipFlipTests/FlipFlipTests.swift
@@ -0,0 +1,34 @@
+//
+//  FlipFlipTests.swift
+//  FlipFlipTests
+//
+//  Created by Bui Quoc Viet on 2/3/20.
+//  Copyright © 2020 Class iOS. All rights reserved.
+//
+
+import XCTest
+@testable import FlipFlip
+
+class FlipFlipTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
