bf513988df08c0e530b155562b722fb17f39515e
vietbq <vietbq@nal.vn>
2020-02-26 15:20:46 +0700

    Inital Project

--- FlipFlip/ViewController.swift
+++ FlipFlip/ViewController.swift
@@ -9,12 +9,36 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var firstView: UIView!
+    var secondView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        firstView = UIView(frame: CGRect(x: 32, y: 32, width: 128, height: 128))
+        secondView = UIView(frame: CGRect(x: 32, y: 32, width: 128, height: 128))
+
+        firstView.backgroundColor = UIColor.red
+        secondView.backgroundColor = UIColor.blue
+
+        secondView.isHidden = true
+
+        view.addSubview(firstView)
+        view.addSubview(secondView)
+
+
+        perform(#selector(flip), with: nil, afterDelay: 2)
     }
 
+    @objc func flip() {
+        let transitionOptions: UIView.AnimationOptions = [.transitionFlipFromLeft]
 
+        UIView.transition(with: firstView, duration: 1.0, options: transitionOptions, animations: {
+            self.firstView.isHidden = true
+        })
+
+        UIView.transition(with: secondView, duration: 1.0, options: transitionOptions, animations: {
+            self.secondView.isHidden = false
+        })
+    }
 }
 
